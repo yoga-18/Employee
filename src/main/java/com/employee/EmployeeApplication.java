65decc953997926a1452aafa0050159ca58e0dc1
yoga-18 <yoganathan.yo@gmail.com>
2023-10-20 10:48:26 +0530

    re-altered

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -8,6 +8,8 @@ public class EmployeeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
+		
+		System.out.println("changes made");
 	}
 
 }
